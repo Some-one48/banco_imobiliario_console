0b93cff2187ac499d1b586ca1c5064cf7ec0f2ef
Some_one <hadassafavari48@gmail.com>
2025-12-27 11:20:40 -0300

    changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import csv
-from random import randint
+from random import randint # pip install
 import os 
 import time
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Back, Style # pip install
+
+players = []
 
 def dado():
     dado = randint(1,6)
@@ -13,40 +15,57 @@ def header():
     print('\tBEM-VINDO AO BANCO IMOBILIÁRIO')
     print('----------------------------------------------')
 
-def newPlayer(players:list):
-    wPlay = []
-    for i in range(len(players)):
+def newPlayer(p:list):
+    for i in range(len(p)):
         player = {
-            'ordem': i+1,
-            'nome': players[i],
+            'ordem': len(players)+1,
+            'casa_atual': 0,
+            'nome': p[len(players)],
             'saldo': 805,
-            'propriedades_id': []
+            'propriedades_position': []
         }
-        wPlay.append(player)
+        players.append(player)
 
-    with open('players.csv', 'w') as file:
-        dados = csv.DictWriter(file, fieldnames=['ordem', 'nome', 'saldo', 'propriedades_id'], lineterminator='\n')
-        dados.writeheader()
+def vezDe(qtd:int, ordem:int):
+    if ordem == 1:
+        return 2
+    else:
+        return 1
 
-        for p in wPlay:
-            dados.writerow(p)
-    
-def vezDe(players:list):
-    atual = 1
-    passado = 0
-    if len(players) == 2:
-        if passado == 1:
-            passado = 1
-            return players[0]
-        else:
-            return
+def acoes(dado:int, o:int):
+    p = players[o-1]
+    saldo = p['saldo']
+
+    p['casa'] += dado
+
+    with open('terrenos.csv', 'r') as file:
+        reader = csv.reader
+        parou = reader[p['casa']]
+
+        print(parou[1])
+        print(f'Preço: {parou[2]}')
+
+    print(f'\nSaldo Atual: {saldo}')
+    print('O que deseja fazer?\n')
+    print('1. Comprar a propriedade')
+    print('2. Ver suas propriedades')
+    print('3. Continuar / Pular')
+    a = int(input('\nDigite sua operação: '))
+
+    if a not in [1,2,3]:
+        print('\n')
+        print(Back.RED + 'ERRO:' + Back.RESET + Fore.RED +'Digite uma operação válida' + Fore.RESET)
+        acoes(2, o)
+    else:
+        print('All right :D')
 
 if __name__ == '__main__':
     finish = False
-    # INICIO
-    header()
 
-    qtd_j = int(input('Digite a quantidade de jogadores: '))
+    # INICIO
+    '''header()
+
+    qtd_j = int(input('Digite a quantpositionade de jogadores: '))
 
     jogadores = []
     for i in range(qtd_j):
@@ -61,13 +80,17 @@ if __name__ == '__main__':
         newPlayer(jogadores)
 
     time.sleep(2)
-    os.system("cls")
+    os.system("cls")'''
 
-    #
-    header()
-    #while(finish == False):
-    print('Vez de', end=' ')
-    print(Back.WHITE + Style.BRIGHT + f'{jogadores[0]}' + Back.RESET + Style.NORMAL)
-    
-    dd = dado()
-    print(f'Dado: {dd}')
+    jogadores = ['Fulano', 'Sicrano']
+    while(finish == False):
+        header()
+        ordem = 0
+        ordem = vezDe(2, ordem)
+        print('Vez de', end=' ')
+        print(Back.WHITE + Style.BRIGHT + f'{jogadores[ordem-1]}' + Back.RESET + Style.NORMAL)
+        
+        dd = dado()
+        print(f'Dado: {dd}')
+
+        acoes(dd, ordem)
